d661b71e12b4f389fd8532177a904106e900a114
Sabrina Mianne and Stephen Trewick <stephen@>
2019-04-23 18:07:20 -0700

    Delete the Test file

--- Program.cs
+++ Program.cs
@@ -2,29 +2,34 @@ using System;
 
 public class Program
 {
-  public static char Translate(char newArray)
+  static void Main()
   {
-    foreach (char letter in newArray)
+    Console.WriteLine("H3ll0, w3lc0m3 70 L337 Sp3ak 7ranzla70r! 7yp3 a s3n73nc3 f0r m3 7o 7ranzla73: ");
+    string UserInput = Console.ReadLine();
+    char[]newArray = UserInput.ToCharArray();
+    for (int i=0; i<newArray.Length; i++)
     {
-      if (letter == 'e')
-      {
-        return '3';
-      }
-
+      char let = newArray[i];
+      if (let == 'e')
+      newArray[i]= '3';
+      else if (let == 'o')
+      newArray[i] = '0';
+      else if (let == 't')
+      newArray[i] = '7';
+      else if (let == 'I')
+      newArray[i] = '1';
     }
-    string result = string.Join("", array);
+    for (int i= 0; i<UserInput.Length; i++)
+    {
+      char let2 = newArray[i];
+      int indexOfS = i;
+      int IndexBeforeS = indexOfS - 1;
+      if(let2 == 's' && indexOfS == 0)
+      newArray[indexOfS] = let2;
+      else if(let2 == 's' && newArray[IndexBeforeS] != ' ')
+      newArray[indexOfS] = 'z';
+    }
+    string result = new string (newArray);
     Console.WriteLine(result);
   }
-
-  public static void Main()
-  {
-    Console.WriteLine("Welc0m3 70 L337 Zp3ak! 3n73r a Z7ring: ");
-    string userInput = Console.ReadLine();
-    char[] newArray = userInput.ToCharArray();
-
-    Translate(newArray);
-    
-  }
-  
 }
-
